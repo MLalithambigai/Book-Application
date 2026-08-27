4180dd696e60aa7e54213ff31a37c9121286107d
MLalithambigai <lalithambigaim22@gmail.com>
2026-08-27 22:30:19 +0530

    Initial commit

---
+++ src/main/java/com/example/BookApplication/Service/BookService.java
@@ -0,0 +1,15 @@
+package com.example.BookApplication.Service;
+
+import com.example.BookApplication.Entity.Book;
+import com.example.BookApplication.Repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+    @Autowired
+    private BookRepository bookRepository;
+    public Book addBook(Book book) {
+        return bookRepository.save(book);
+    }
+}
